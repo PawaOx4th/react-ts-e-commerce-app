534b47cad55b15c8b06db4cc4a0c296fc54b4156
pawaox4th <pawaox4th@gmail.com>
2023-01-17 23:07:49 +0700

    feat: 🎸 initial router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
 import { useState } from "react"
 import reactLogo from "./assets/react.svg"
-import "./style/App.css"
+import { Routes, Route } from "react-router-dom"
+
+function HomePage() {
+  return (
+    <div>
+      <h1>This is a homepage!</h1>
+    </div>
+  )
+}
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>This is a AboutPage!</h1>
+    </div>
+  )
+}
+
+function ErrorPage() {
+  return (
+    <div>
+      <h2>
+        <i>404 Page not found</i>
+      </h2>
+    </div>
+  )
+}
 
 function App() {
-  return <div className="App">Hello world!</div>
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/*  http://localhost:5172/about */}
+        <Route path="about" element={<AboutPage />} caseSensitive />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
