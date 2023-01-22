e425e64e16a8ceb8ad65552d7a379045ae2c746b
pawaox4th <pawaox4th@gmail.com>
2023-01-22 16:33:57 +0700

    refactor: 💡 Custom bgColor on Layout container

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,18 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
+import styled from "styled-components"
 import Container from "./Container"
 import Navbar from "./Navbar"
 
-type Props = {}
+const LayoutStyle = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: ${(props) => props.theme.color.white};
+`
 
-function Layout({}: Props) {
+type LayoutPropsType = {}
+
+function Layout({}: LayoutPropsType) {
   return (
     <div>
       <Navbar />
