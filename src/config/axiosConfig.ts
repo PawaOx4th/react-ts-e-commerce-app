bac1488222881123f17501200506e5c80392e9de
Kaen Pawanachai <pawaox4th@gmail.com>
2023-02-02 11:05:18 +0700

    Remove todo

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,6 +1,5 @@
 import axios from "axios"
 
-// @todo #1 Implement responsive layout and navigation
 const clientApi = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
