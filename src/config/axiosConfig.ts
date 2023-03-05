d972425d21f5aaa113aeb47b1c774013725b44d2
Kaen Pawanachai <pawaox4th@gmail.com>
2023-03-05 12:07:53 +0700

    feat: 🎸 Add axios request and response interceptors and dayjs d

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,7 +1,26 @@
 import axios from "axios"
 
-const client = axios.create({
+const axiosApiInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 })
 
+axiosApiInstance.interceptors.request.use(
+  (config) => {
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
+axiosApiInstance.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
+
+const client = axiosApiInstance
 export default client
