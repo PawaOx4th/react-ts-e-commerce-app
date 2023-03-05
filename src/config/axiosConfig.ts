5ea0a51c88661ad826c544df530a53da74c3ac61
Kaen Pawanachai <pawaox4th@gmail.com>
2023-03-05 12:54:09 +0700

    refactor: 💡 Add authorization header with JWT token if present in local storage

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,4 +1,6 @@
 import axios from "axios"
+import * as dayjs from "dayjs"
+import jwtDecode from "jwt-decode"
 
 const axiosApiInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -6,6 +8,24 @@ const axiosApiInstance = axios.create({
 
 axiosApiInstance.interceptors.request.use(
   (config) => {
+    const secret = localStorage.getItem("secret")
+    const token = JSON.parse(secret ?? "") as { state?: { jwt?: string } }
+    if (secret && token?.state?.jwt) {
+      const jwtData = jwtDecode<{ id: number; iat: number; exp: number }>(
+        token?.state?.jwt,
+      )
+
+      // TODO:  Check is expired.
+      const isExpired = dayjs.unix(jwtData.exp).diff(dayjs()) < 1
+
+      if (!isExpired) {
+        config.headers["Authorization"] = `Bearer ${token.state.jwt}`
+        return config
+      }
+
+      return config
+    }
+
     return config
   },
   (error) => {
